f788c59a772bdcd30a42150509e767a0a0c56fc4
VladimirProgrammer <vova_progmush@mail.ru>
2021-01-10 18:36:16 +0300

    first commit

---
+++ README.m
@@ -0,0 +1,4 @@
+# COLMAR-ACADEMY
+I have eventually finished this project called “Colmar Academy”. This project is a part of the career path “Build a Website with HTML, CSS and Github Pages” by Codecademy. 
+The codes (HTML and CSS) have been divided into sections so that it can be easier to read them. Flexbox has been used to position the elements. Although it is supposed by the task to apply responsive design, up for now I haven't done this in order not to make the code more complicated. The semantic has been applied here but not a lot. 
+10.01.2021 Vladimir
